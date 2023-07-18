88920bf091c194cb1d093e181e25f256bb177f09
WinstonMcleod3580 <45207073+WinstonMcleod3580@users.noreply.github.com>
2023-07-17 23:23:01 -0500

    Add files via upload

---
+++ connector/DBconnector.java
@@ -0,0 +1,27 @@
+package connector;
+
+import java.sql.*;				
+import javax.swing.JOptionPane;
+
+
+public class DBconnector {
+	
+	private static Connection conn=null;
+	
+	public static Connection getDatabaseConnection() {
+		if(conn==null) {
+			String url="jdbc:mysql://localhost:3306/wodda";			
+			try {
+				conn=DriverManager.getConnection(url, "root", "Qwerty12345");
+				if(conn !=null) {
+					JOptionPane.showMessageDialog(null, "Connected to Local Server","JDBC Connection Status",JOptionPane.INFORMATION_MESSAGE);
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return conn;
+	}
+
+}
