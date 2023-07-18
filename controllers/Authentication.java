5c4d965f0e9a42cf696f5ec8da6099285ff3c189
AStewart21 <93847247+AStewart21@users.noreply.github.com>
2023-07-17 20:00:57 -0500

    Update Authentication.java
    Updated authentication for customer

--- controllers/Authentication.java
+++ controllers/Authentication.java
@@ -1,9 +1,36 @@
 package controllers;
+import connector.Database;
+import connector.Database.DatabaseConnection;
 
 public class Authentication {
-    public boolean loginCustomer(String customerId, String password) {
+    
+    private Connection conn;
+     // Constructor
+    public Authentication(Connection conn) {
+        this.conn = conn;
+    }
+
+    public Authentication() {
+        // Get the database connection using DatabaseConnection
+        conn = DatabaseConnection.getDatabaseConnection();
+    }
+    public boolean loginCustomer(String customerId, String password)
+            {
+        try {
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE customer_id = ?")) {
+            statement.setInt(1, customerId);
+             }
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String storedPassword = resultSet.getString("password");
+                    return password.equals(storedPassword);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return false;
-        //authenticate customer
     }
 
     public boolean loginEmployee(String staffId, String password) {
