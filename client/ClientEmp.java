9600fb3e2aadc7e6744eb111a6dc2f34da0e599e
Daniel Eccleston <60997252+Liquid-Nitro@users.noreply.github.com>
2023-07-20 18:14:25 -0500

    setup employee client and driver

--- client/ClientEmp.java
+++ client/ClientEmp.java
@@ -1,5 +1,102 @@
 package client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+import view.CustomerView;
+
 public class ClientEmp {
-    
+    private ObjectOutputStream os;
+	private ObjectInputStream is;
+	private Socket connection;
+    private String action;
+
+    public ClientEmp(){
+        this.createconnection();
+        this.getStreams();
+     }
+
+     private void createconnection() {
+		try{
+							
+			connection = new Socket("127.0.0.1",8888);
+			
+		}catch(IOException ex){
+		    ex.printStackTrace();
+		}
+	}
+
+    public final void getStreams(){
+		try{
+			
+			os = new ObjectOutputStream(connection.getOutputStream());
+			is = new ObjectInputStream(connection.getInputStream());
+			
+		}catch(IOException ex){
+		    ex.printStackTrace();
+		}
+	}	
+	public void closeConnection() {
+		try {
+			
+			os.close();
+			is.close();
+			connection.close();
+			
+		}catch(IOException ex){
+		   ex.printStackTrace();
+		}
+	}
+
+    public void sendAction(String action){
+        this.action = action;
+        try{
+            os.writeObject(action);
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendEmployee(Employee emp){
+		try{
+		  
+		  os.writeObject(emp);
+		  
+		}
+		catch(IOException ex){
+		  ex.printStackTrace();
+		}
+	}
+
+
+
+    public void receiveResponse(){
+		 try{
+            if(action.equalsIgnoreCase("Add Employee")){
+                Boolean flag = (Boolean) is.readObject();
+                if (flag == true){
+                    JOptionPane.showMessageDialog(null,"Record added successfully","Add Record Status",JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        
+            if(action.equalsIgnoreCase("Find Employee")){
+                CustomerView customer = new CustomerView();
+                customer = (CustomerView) is.readObject();
+                if (customer == null){
+                    JOptionPane.showMessageDialog(null,"Record could not be found","Find Record Status",JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }
+        }catch(ClassCastException ex){
+            ex.printStackTrace();
+        }catch(ClassNotFoundException ex){
+            ex.printStackTrace();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+	}
 }
--- client/Employee.java
+++ client/Employee.java
@@ -1,211 +1,96 @@
 package client;
 
+import java.io.Serializable;
 
+public class Employee implements Serializable {
+    private String staffId;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String contactNumber;
+    private String role;
 
-import java.awt.HeadlessException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.table.DefaultTableModel;
-
-import view.CustomerView;
-
-
- 
-public class Employee {
-    // frame
-    JFrame f;
-    // Table
-    JTable j;
-    private Connection con;
-	ResultSet result, result1 = null;
-	Statement stat, stat1 = null;
- 
-    // Constructor
-    Employee()
-    {
-    	
-    	    	
-    	 JFrame login = new JFrame("Employee Login");
-    	 login.setSize(400,400);
-    	 JButton logg = new JButton("Login");
-    	 JLabel cusid =new JLabel ("Employee ID:");
-	     cusid.setBounds(30,10, 100,30); 
-	     JLabel pword =new JLabel ("Password:");
-	     pword.setBounds(30,50, 100,30); 
-	     JTextField enterid = new JTextField();
-	     JTextField enterpword = new JTextField();
-	     enterid.setBounds(130,10, 200,30);  
-	     enterpword.setBounds(130,50, 200,30);  
-    	 logg.setBounds(30,120,100,30);
-    	 
-    	 login.add(cusid);login.add(pword);login.add(enterid);login.add(enterpword);login.add(logg);
-    	 
-    	 login.setLayout(null);
-	     login.setVisible(true);
-	     login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
-	     logg.addActionListener(new ActionListener() {
-	            @Override
-	            public void actionPerformed(ActionEvent e) {
-	            	String sql2 ="SELECT password FROM complaint.emplogin WHERE id ='"+ enterid.getText() + "'" ;
-            		
-            		
-            		Statement state;
-            		ResultSet res;
-            		try {
-            			state = con.createStatement();
-            			res = state.executeQuery(sql2);
-            			String password = null;
-            			if (res.next()) {
-                			
-                			password = res.getString("password");
-                		}
-							if(password.equals(enterpword.getText()) ){
-								dashboard();
-							}else {
-								 JOptionPane.showMessageDialog(login, "Incorrect Login Try Again","Login Error", JOptionPane.INFORMATION_MESSAGE);
-							        enterid.setText("");
-							        enterpword.setText("");
-							 }
-            		}catch (HeadlessException | SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}	}
-	        });
+   
+    public Employee() {
     }
-    
-    public void dashboard() {
-    	
-    	
-    	JFrame frame = new JFrame("Customer Complaints");
-    	frame.setSize(400,400);
-    	JButton button = new JButton("View Complaint");
-        JButton button1 = new JButton("Schedule a Technician");
-        JButton button2 = new JButton("View Completed Responses");
-        
-        button.setBounds(130,50,150,60);
-        button1.setBounds(110,150,200,60);
-        button2.setBounds(130,250,150,60);
-        
-        frame.add(button);frame.add(button1);
-        frame.add(button2);
-        
-        frame.setLayout(null);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               		viewall();
-            	
-            	 }	                      	                         
-        });
-    	
-    	         
+
+
+
+    public Employee(String staffId,String username,String password,String firstName,String lastName,String email,String contactNumber, String role) {
+        this.staffId=staffId;
+        this.password=password;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+        this.contactNumber=contactNumber;
+        this.role=role;
     }
-    
-    
-    public void viewall() {
-    	 // Frame initialization
-    	//con = MainServer.getDatabaseConnection();
-    	
-    	String sql ="SELECT * FROM complaint.customer";
-    	String sql2 ="SELECT * FROM complaint.complaint";
-    	
-      JFrame  f = new JFrame();
-        JTable j = new JTable();
-        JButton back = new JButton("Back");
-        JButton response = new JButton("Respond");
-        
-        back.setBounds(10,200, 100,30);
-        response.setBounds(130,200,100,30);
-      // Frame Title
-        f.setTitle("Complaint List");
-        String[] columnNames = { "ID", "First Name", "Last Name" , "Email", "Contact Number", "Nature","Complaint" };
-        // Data to be displayed in the JTable
-        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-        CustomerView c = new CustomerView();
-		try {
-    		stat = con.createStatement();
-    		result = stat.executeQuery(sql);
-    		
-    		stat1 = con.createStatement();
-    		result1 = stat1.executeQuery(sql2);
-    		
-    		 while (result.next() && result1.next()) {
-    			 model.addRow(new Object[]{result.getString("customerid"),result.getString("firstname"), result.getString("lastname"), result.getString("email"), result.getString("contact"), result1.getString("nature"), result1.getString("complaint")});   
-    			 
-    		 }			
 
-    	} catch (SQLException e1) {
-    		// TODO Auto-generated catch block
-    		e1.printStackTrace();
-    	}
-       
-        // Column Names
-		f.add(back); f.add(response);
-        j.setModel(model);
 
-        j.setBounds(30, 40, 200, 300);
- 
-        // adding it to JScrollPane
-        JScrollPane scrollPane = new JScrollPane(j);
-        f.add(scrollPane);
-        // Frame Size
-        f.setSize(1000, 300);
-        // Frame Visible = true
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public String getstaffId() {
+        return this.staffId;
     }
-    
-   public void complaintresponse() {
-	   //con = MainServer.getDatabaseConnection();
-   	
-   		String sql ="SELECT * FROM complaint.customer";
-   		
-   		try {
-    		   		
-    		stat1 = con.createStatement();
-    		result1 = stat1.executeQuery(sql);
-    		
-    		String info = null;
-    		int i = 0;
-    		 String type[]={};        
-    		
-    		 while (result1.next()) {
-    			 type[i] = result1.getString("customerid");
-    			 i++;
-    		 }			
-    		 JComboBox complaint = new JComboBox(type);
-    		 
-    		 
 
-    	} catch (SQLException e1) {
-    		// TODO Auto-generated catch block
-    		e1.printStackTrace();
-    	}
-   		
-   		
-   }
+    public void setstaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNumber() {
+        return this.contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     
-    public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		new Employee();
-	}
+    public String getRole() {
+        return this.role;
+    }
+
+
+
+    public void setRole(String string) {
+        this.role = string;
+    }
+
+    
+
+   
+
 
 }
+
